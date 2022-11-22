f4c5addc9272dacb92220c93ad584520d903d886
Edozie <edoziechukwuma@gmail.com>
2022-11-22 09:13:47 -0600

    Create routes and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Missions from './pages/Missions';
+import Rockets from './pages/Rockets';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 function Navbar() {
-  return (  );
+  return (
+    <header>
+      <h1>Space Travelers Hub</h1>
+      <nav>
+        <Link to="/">Rockets</Link>
+        <Link to="/missions">Missions</Link>
+        <Link to="/profile">My Profile</Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
