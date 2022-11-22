b32dbb09f23024f2075199af4bfc2ee4b53e5c9f
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-22 14:20:52 -0600

    Set up table to receive data from API

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import '../styles/missions.css';
 
 const Missions = () => (
-  <div>
-   
+  <div className="table-container">
+    <table>
+      <thead className="column-header">
+        <tr className="column-row">
+          <th className="column-data">Mission</th>
+          <th className="column-data">Description</th>
+          <th className="column-data">Membership</th>
+          <th className="column-data">Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="column-row">
+          <td className="column-data-body">1</td>
+          <td className="column-data-body">1</td>
+          <td className="column-data-body">
+            <div className="membership-stat">
+              Not A Member
+            </div>
+          </td>
+          <td className="column-data-body">
+            <button type="button">
+              Join Mission
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 );
-
 export default Missions;
