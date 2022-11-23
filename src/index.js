baaaeae879b43f2ff3b12895ea7af6888c0294d6
Edozie <edoziechukwuma@gmail.com>
2022-11-23 09:33:08 -0600

    Correct linter errors

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-
-
+// import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
