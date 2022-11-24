559f1511bac9c26ffe0be5d21290f5bd46bac21a
Edozie <edoziechukwuma@gmail.com>
2022-11-24 10:05:15 -0600

    Add reserve button

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,16 +1,46 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { rocketReserve } from '../redux/rockets/rocketSlice';
 
-const Rocket = () => (
-  <div>
+const Rocket = (props) => {
+  const { rocketelement } = props;
+  const dispatch = useDispatch();
+  return (
     <div>
-      <img src="" alt="" />
+      <div>
+        <img src={rocketelement.flickr_images[0]} alt="" />
+      </div>
+      <div>
+        <h2>{rocketelement.rocket_name}</h2>
+        <div>
+          <span>
+            {rocketelement.reserved ? (
+              <span className="badge">Reserved</span>
+            ) : (
+              ''
+            )}
+          </span>
+          <p>{rocketelement.description}</p>
+        </div>
+        <input
+          type="button"
+          onClick={() => dispatch(rocketReserve(rocketelement.rocket_id))}
+          value={rocketelement.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
+        />
+      </div>
     </div>
-    <div>
-      <h2>title</h2>
-      <p>hello hello </p>
-      <input type="button" value="Reserve Rocket" />
-    </div>
-  </div>
-);
+  );
+};
+
+Rocket.propTypes = {
+  rocketelement: PropTypes.shape({
+    rocket_id: PropTypes.string,
+    rocket_name: PropTypes.string,
+    description: PropTypes.string,
+    flickr_images: PropTypes.arrayOf(PropTypes.string),
+    reserved: PropTypes.bool,
+  }).isRequired,
+};
 
 export default Rocket;
--- src/redux/rockets/rocketSlice.js
+++ src/redux/rockets/rocketSlice.js
@@ -8,13 +8,27 @@ const initialState = {
 
 const fetchData = createAsyncThunk('RESERVE_ROCKETS', async () => {
   const getData = Axios.get('https://api.spacexdata.com/v3/rockets');
+  const rocketList = await getData.json();
+  return rocketList;
 });
 
 const rocketSlice = createSlice({
   name: 'GetrocketSlice',
   initialState,
-  reducers: {},
-  extraReducers: {},
+  reducers: {
+    rocketReserve: (state, action) => state.map((rocket) => {
+      if (rocket.rocket_id === action.payload) {
+        return { ...rocket, reserved: !rocket.reserved };
+      }
+      return rocket;
+    }),
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchData.fulfilled, (state, action) => action.payload);
+  },
 });
 
-export default rocketSlice.reducer;
+const { actions, reducer } = rocketSlice;
+const { rocketReserve } = actions;
+export { actions, fetchData, rocketReserve };
+export default reducer;
