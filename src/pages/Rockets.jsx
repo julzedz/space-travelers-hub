39f7fb5f34dba67c42a471bd721cb9e315ff1fc8
Edozie <edoziechukwuma@gmail.com>
2022-11-25 21:40:18 -0600

    edit Rockets.jsx page

--- src/pages/Rockets.jsx
+++ src/pages/Rockets.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Rocket from '../components/Rocket';
+import {
+  selectAllRockets, fetchRockets,
+} from '../redux/rockets/rocketSlice';
 
-const Rockets = () => (
-  <>
-    <h1>Rockets</h1>
-    <Rocket />
-  </>
-);
+let didInitialRender = false;
+const Rockets = () => {
+  const rockets = useSelector(selectAllRockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!didInitialRender) {
+      dispatch(fetchRockets());
+      didInitialRender = true;
+    }
+  }, []);
+
+  return (
+    <div className="rockets">
+      {rockets.map((rocket) => (
+        <Rocket key={rocket.id} rocket={rocket} />
+      ))}
+    </div>
+  );
+};
 
 export default Rockets;
